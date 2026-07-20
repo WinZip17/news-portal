9ba3eed31e699fc0113d82208c87656a91e27588
Aushev Juriy <wydo17@yandex.ru>
2026-07-20 14:53:47 +0300

    add prod target

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -65,7 +65,8 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        // target: 'http://localhost:3001',
+        target: 'https://short-news.ru/',
         changeOrigin: true,
       },
     },
