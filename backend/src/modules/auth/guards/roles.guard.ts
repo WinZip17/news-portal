65071ec9f4d8b1481a9efa1c282a01f45c28cbd0
Aushev Juriy <wydo17@yandex.ru>
2026-07-21 23:22:37 +0300

    fix type roles guard

--- backend/src/modules/auth/guards/roles.guard.ts
+++ backend/src/modules/auth/guards/roles.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { ROLES_KEY } from '../decorators/roles.decorator';
+import { User } from '../../../entities';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
@@ -13,7 +14,7 @@ export class RolesGuard implements CanActivate {
       return true;
     }
 
-    const { user } = context.switchToHttp().getRequest();
+    const { user }: { user: User } = context.switchToHttp().getRequest();
     return requiredRoles.includes(user.role);
   }
 }
