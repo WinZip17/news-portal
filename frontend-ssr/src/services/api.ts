8c4ea216026472c6a1247985bcbebbb4e9f73a1e
Aushev Juriy <wydo17@yandex.ru>
2026-07-23 20:37:19 +0300

    upd getBaseUrl

--- frontend-ssr/src/services/api.ts
+++ frontend-ssr/src/services/api.ts
@@ -1,13 +1,16 @@
 import axios from 'axios';
 import { isBrowser } from '../utils/isBrowser';
 
-const baseURL =
-  process.env.NODE_ENV !== 'development' && !isBrowser()
-    ? 'http://localhost:3001/api'
-    : '/api';
+const getBaseUrl = () => {
+  if (isBrowser()) return '/api';
+  if (process.env.NODE_ENV === 'development')
+    return 'http://localhost:3001/api';
+  // На сервере в Docker используем имя сервиса
+  return 'http://backend:3001/api';
+};
 
 const api = axios.create({
-  baseURL,
+  baseURL: getBaseUrl(),
   timeout: 10000,
   headers: { 'Content-Type': 'application/json' },
 });
