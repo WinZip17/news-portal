5accdc87a280f16fbb2c9f5a172e9ea4315527cc
Aushev Juriy <wydo17@yandex.ru>
2026-07-25 22:25:32 +0300

    upd serviceWorker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
+      injectRegister: null,
       includeAssets: ['favicon.svg', 'apple-touch-icon.png'],
       manifest: {
         name: 'News Portal - Короткие новости без манипуляций',
