972288ce5c832130d5403fdb6a0e130f54036fc0
Aushev Juriy <wydo17@yandex.ru>
2026-07-28 18:10:36 +0300

    fix ym

--- frontend/src/components/YandexMetrika.tsx
+++ frontend/src/components/YandexMetrika.tsx
@@ -4,24 +4,16 @@ const YandexMetrika: React.FC<{ counterId: number }> = ({ counterId }) => {
   useEffect(() => {
     if (!import.meta.env.DEV) {
       const script = document.createElement('script');
-      script.src = `https://mc.yandex.ru/metrika/tag.js?id=${counterId}`;
-      script.async = true;
+      script.innerHTML = `
+      (function(m,e,t,r,i,k,a){
+      m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+      m[i].l=1*new Date();
+      for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+      k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+      })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=${counterId}', 'ym');
+      ym(${counterId}, 'init', {ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true, host:"mc.yandex.ru"});
+    `;
       document.head.appendChild(script);
-      const initScript = document.createElement('script');
-      initScript.innerHTML = `
-        ym(${counterId}, 'init', {
-          ssr: true,
-          webvisor: true,
-          clickmap: true,
-          ecommerce: "dataLayer",
-          referrer: document.referrer,
-          url: location.href,
-          accurateTrackBounce: true,
-          trackLinks: true,
-          host: "mc.yandex.ru"
-        });
-      `;
-      document.head.appendChild(initScript);
     }
   }, [counterId]);
 
