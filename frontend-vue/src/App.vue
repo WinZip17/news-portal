48d17ab869ee24d4bcfdb6e68b239146a5318079
Aushev Juriy <wydo17@yandex.ru>
2026-08-02 17:17:22 +0300

    fix Vuetify Theme

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -8,4 +8,20 @@
 
 <script setup lang="ts">
 import MainLayout from '@/layouts/MainLayout.vue';
+
+import { watch } from 'vue';
+import { useTheme } from 'vuetify';
+import { useUIStore } from '@/stores/ui';
+
+const vuetifyTheme = useTheme();
+const uiStore = useUIStore();
+
+vuetifyTheme.global.name.value = uiStore.theme;
+
+watch(
+  () => uiStore.theme,
+  (newTheme) => {
+    vuetifyTheme.global.name.value = newTheme;
+  }
+);
 </script>
--- frontend-vue/src/plugins/vuetify.ts
+++ frontend-vue/src/plugins/vuetify.ts
@@ -1,18 +1,15 @@
 import 'vuetify/styles';
 import '@mdi/font/css/materialdesignicons.css';
-
 import { createVuetify } from 'vuetify';
-
 import { darkTheme, lightTheme } from './theme';
-import { ThemeName } from '@/constants/theme';
 
 export function createAppVuetify() {
   return createVuetify({
     theme: {
-      defaultTheme: ThemeName.Light,
+      defaultTheme: 'light',
       themes: {
-        [ThemeName.Light]: lightTheme,
-        [ThemeName.Dark]: darkTheme
+        light: lightTheme,
+        dark: darkTheme
       }
     }
   });
